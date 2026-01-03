906801bcd2436dc4a74c00cc811e1d47ebc552c9
Abhishek <adveritoprojects@gmail.com>
2026-01-03 23:34:17 +0530

    changed: next config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,13 @@ const nextConfig = {
         hostname: "cdn.pixabay.com",
       },
     ],
-    qualities: [75, 100],
+
+    // 👇 ADD THESE
+    deviceSizes: [360, 414, 640, 768, 1024, 1280, 1536, 1920, 2560, 3840],
+
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+
+    qualities: [75, 90, 100],
   },
 };
 
